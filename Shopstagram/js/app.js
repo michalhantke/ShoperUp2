6f0fe058b562fc45876fd7a3ff5c3b5deaef046d
michalhantke <michal.hantke@shoper.pl>
2023-01-28 13:09:51 +0100

    object person

--- Shopstagram/js/app.js
+++ Shopstagram/js/app.js
@@ -7,3 +7,23 @@ for (let i=0; i < tab2.length; i++) {
     tab3.push(tab2[i].split(" "));
 }
 console.log(tab3)
+
+const person = {
+    name: "Andrzej",
+    surname: "Nowak",
+    age: 36,
+    favouriteMovies: ["Rambo", "Rambo II", "Rambo III"],
+    getFavMovies: function() {
+        return this.favouriteMovies.join(", ");
+    },
+    getFullName: function() {
+        return (`${this.name} ${this.surname}`);
+    },
+    sayHello: function() {
+        return (`Hello my name is ${this.getFullName()}, I am ${this.age}, and my favorite movies are ${this.getFavMovies()}.`);
+    }
+};
+
+console.log(person.sayHello());
+
+
